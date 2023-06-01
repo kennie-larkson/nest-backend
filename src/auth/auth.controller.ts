da2a679b59735f9c8031e751d79e65777cb69d2d
Kehinde Lawal <kehindelawal@tts-MacBook-Pro.local>
2023-06-01 22:40:52 +0100

    (feat): user can only access protected routes with the correct authorization token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,9 +5,12 @@ import {
   Post,
   HttpCode,
   HttpStatus,
+  UseGuards,
+  Request,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/signin.dto';
+import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -17,5 +20,11 @@ export class AuthController {
   @Post('login')
   signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto.email, signInDto.password);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return `Congratulations ${req.user.name}. You have accesed a protected route.`;
   }
 }
